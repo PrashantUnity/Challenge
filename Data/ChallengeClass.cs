0f2a8cfb7c8bc334e1596273d50ae2eb2cf62ea5
Prashant Priyadarshi <48068632+PrashantUnity@users.noreply.github.com>
2024-03-05 13:42:56 +0530

    Add project files.

---
+++ Data/ChallengeClass.cs
@@ -0,0 +1,19 @@
+﻿namespace Challenge.Data;
+
+public class ChallengeClass
+{
+    public int Id { get; set; }
+    public bool HealthyBreakFast { get; set; }
+    public bool DailyTask { get; set; }
+
+    public bool PostOnline { get; set; }
+
+    public bool Learn { get; set; }
+
+    public bool WriteCode { get; set; }
+
+    public bool Exercise { get; set; }
+    public bool Clean { get; set; }
+
+    public int Day { get; set; }
+}
---
+++ Data/Repository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Challenge.Data
+{
+    public class Repository : DbContext
+    {
+        public Repository(DbContextOptions<Repository> opts) : base(opts)
+        {
+
+        }
+
+        public DbSet<ChallengeClass> Challenges { get; set; } = null!;
+    }
+}
